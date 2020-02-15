f0e9eddce125aa0e63eed87d2482dd846f09ece5
Joze Rihtarsic <jrihtarsic1@gmail.com>
2020-02-15 12:33:44 +0100

    fix xstl plugin

---
+++ scripts/test-deploy-web.sh
@@ -0,0 +1,14 @@
+#/!bin/sh
+
+
+
+WILDFLY_HOME="wildfly-11.0.0.Final"
+BOUNDLE_TEST="Laurentius-test"
+FOLDER_DEPLOY="test-deploy"
+
+cp ../Laurentius-app/Laurentius-web/target/laurentius-web.war $FOLDER_DEPLOY/$WILDFLY_HOME/standalone/deployments/
+
+
+cd "$FOLDER_DEPLOY/$WILDFLY_HOME/bin"
+
+./laurentius-init.sh --init -d mb-laurentius.si
